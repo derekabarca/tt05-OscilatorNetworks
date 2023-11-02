34c124097800e2af42a464ea50be5898ce2b1d29
Derek Abarca <dabarca2@ucsc.edu>
2023-11-02 16:25:42 -0700

    yeah

--- src/tt_um_topLevel_derekabarca.v
+++ src/tt_um_topLevel_derekabarca.v
@@ -1,20 +1,17 @@
 `default_nettype none
 
 module tt_um_topLevel_derekabarca (
-    input  wire [7:0] ui_in,    // Dedicated inputs - connected to the input switches
-    output wire [7:0] uo_out,   // Dedicated outputs - connected to the 7 segment display
-    input  wire [7:0] uio_in,   // IOs: Bidirectional Input path
-    output wire [7:0] uio_out,  // IOs: Bidirectional Output path
-    output wire [7:0] uio_oe,   // IOs: Bidirectional Enable path (active high: 0=input, 1=output)
-    input  wire       ena,      // will go high when the design is enabled
-    input  wire       clk,      // clock
-    input  wire       rst_n,     // reset_n - low to reset
-
-    output wire spike_output
+  input  wire [7:0] ui_in,    // Dedicated inputs - connected to the input switches
+  output wire [7:0] uo_out,   // Dedicated outputs - connected to the 7 segment display
+  input  wire [7:0] uio_in,   // IOs: Bidirectional Input path
+  output wire [7:0] uio_out,  // IOs: Bidirectional Output path
+  output wire [7:0] uio_oe,   // IOs: Bidirectional Enable path (active high: 0=input, 1=output)
+  input  wire       ena,      // will go high when the design is enabled
+  input  wire       clk,      // clock
+  input  wire       rst_n,     // reset_n - low to reset
+  output wire spike_output
 );
-
-    // topLevel logic
-
+  // topLevel logic
   wire reset = !rst_n;
   wire spike_neuron1, spike_neuron2;  // spike signals from neuron modules
 
