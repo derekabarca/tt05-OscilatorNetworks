183b95a9197717806f6c4593df044991ece8c27c
Derek Abarca <dabarca2@ucsc.edu>
2023-11-03 16:30:43 -0700

    Updating counter + logic in synapse.v

--- src/synapse.v
+++ src/synapse.v
@@ -35,6 +35,38 @@
 // endmodule
 
 
+
+
+
+
+
+// module synapse (
+//   input wire spike_input,   // spike input from the presynaptic neuron
+//   output wire spike_output  // spike output to the postsynaptic neuron
+// );
+
+//   reg [1:0] delay_counter;   // counter for delay
+//   wire default_spike_output; // Default value for spike_output
+
+//   // default assignment
+//   assign default_spike_output = 1'b0;
+
+//   always @(posedge spike_input) begin
+//     delay_counter <= 2'b00;
+//   end
+
+//   // assign spike_output a default value if not assigned in the always block
+//   assign spike_output = (delay_counter == 2'b00) ? default_spike_output : 1'b0;
+
+// endmodule
+// syanpse.v code block above passes, but it is not incrementing delay_counter at all...  new code below:
+
+
+
+
+
+
+
 module synapse (
   input wire spike_input,   // spike input from the presynaptic neuron
   output wire spike_output  // spike output to the postsynaptic neuron
@@ -47,10 +79,14 @@ module synapse (
   assign default_spike_output = 1'b0;
 
   always @(posedge spike_input) begin
-    delay_counter <= 2'b00;
+    if (delay_counter < 2'b11) begin
+      delay_counter <= delay_counter + 1;
+    end
   end
 
   // assign spike_output a default value if not assigned in the always block
-  assign spike_output = (delay_counter == 2'b00) ? default_spike_output : 1'b0;
-
+  assign spike_output = (delay_counter == 2'b11) ? default_spike_output : 1'b0;
+// delay_counter is incremented when a positive edge of spike_input is detected. 
+  // increases from 2'b00 to 2'b11, which represents a 2-cycle delay. 
+    // when delay_counter reaches 2'b11,  spike_output is set to 1'b0, indicating that the delayed spike has passed through.
 endmodule
