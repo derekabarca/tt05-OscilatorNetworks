4768beb77477f12f5b73596f8b176b8e1fba60e0
Derek Abarca <dabarca2@ucsc.edu>
2023-11-04 03:22:50 -0700

    Real Final Push

--- src/tt_um_topLevel_derekabarca.v
+++ src/tt_um_topLevel_derekabarca.v
@@ -6,8 +6,7 @@ module tt_um_topLevel_derekabarca (
   output wire [7:0] uio_oe,   // IOs: Bidirectional Enable path (active high: 0=input, 1=output)
   input  wire       ena,      // will go high when the design is enabled
   input  wire       clk,      // clock
-  input  wire       rst_n,     // reset_n - low to reset
-  output wire spike_output
+  input  wire       rst_n     // reset_n - low to reset
 );
   // topLevel logic
   wire reset = !rst_n;
@@ -24,11 +23,10 @@ module tt_um_topLevel_derekabarca (
 
   // output of the synapse modules becomes the network's spike output
   assign spike_output = temp_wire_synapse1out || temp_wire_synapse2out;
+  assign uo_out = {7'b0, spike_output}; 
+
 
   // connect the unused pins to Neuron or Synapse modules
-  // assign ui_in = 8'b0;           // no specific input from switches
-  assign uo_out = 8'b0;          // no specific output to 7-segment display
-  // assign uio_in = 8'b0;          // no specific bidirectional input
   assign uio_out = 8'b0;         // no specific bidirectional output
   assign uio_oe = 8'b0;          // no specific bidirectional enable path
 
